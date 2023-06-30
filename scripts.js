cbb55a72d880d27dc06713fffd18a2fdf8ebb93c
Matt Born <mattborn@users.noreply.github.com>
2023-06-30 15:08:13 -0600

    Metadata and mobile tweaks

--- scripts.js
+++ scripts.js
@@ -1,11 +1,9 @@
-let suppress = false
 document.querySelectorAll('.card').forEach(card => {
-  card.addEventListener('mouseenter', e => {
-    suppress = true
+  card.addEventListener('pointerenter', e => {
     document.body.classList.add('invert')
     document.body.style.backgroundColor = card.dataset.hex
   })
-  card.parentNode.addEventListener('mouseleave', e => {
+  card.parentNode.addEventListener('pointerleave', e => {
     document.body.classList.remove('invert')
     document.body.style.backgroundColor = 'white'
   })
